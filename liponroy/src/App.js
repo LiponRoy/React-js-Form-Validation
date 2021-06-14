f1dbed207fb7bb0b55e748933d3c5ad6b1f2b82c
LiponRoy <liponbdbd@gmail.com>
2021-06-14 14:08:44 +0600

    first commit

---
+++ liponroy/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+// for bootstrap
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import From from './component/From';
+
+function App() {
+	return (
+		<div className='App'>
+			<From></From>
+		</div>
+	);
+}
+
+export default App;
